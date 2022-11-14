d21fb67d07b67315b24c684cbead9725e859df73
pnpsuM <89827636+pnpsuM@users.noreply.github.com>
2022-11-14 11:15:49 +0900

    baseline

---
+++ model/model.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from keras.models import Model, Sequential
+from keras.layers import Dense, Input, Dropout, BatchNormalization
+
+param = {'D0':13, 'D1':8, 'Dropout': 0.2}
+
+def BinaryClassification(input_shape = None):
+    inputs = Input(input_shape = input_shape, name = 'Input')
+    x = Dense(param['D0'], activation='leaky_relu', name = 'Dense0')(inputs)
+    x = Dropout(param['Dropout'])(x)
+    x = Dense(param['D1'], activation='leaky_relu', name = 'Dense1')(x)
+    outputs = Dense(1, activation='sigmoid', name = 'Output')(x)
+    model = Model(inputs = inputs, outputs = outputs, name = 'TitanicBC')
+    model.compile(loss = 'binary_crossentropy', optimizer='adam')
+    model.summary()
+    
+    return model
