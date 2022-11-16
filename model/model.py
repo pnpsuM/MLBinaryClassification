12eb92faebfa4b74e389f1dd6bc87ff805782e37
pnpsuM <89827636+pnpsuM@users.noreply.github.com>
2022-11-16 16:28:58 +0900

    Minor addition for modularization

--- model/model.py
+++ model/model.py
@@ -5,16 +5,16 @@ import tensorflow as tf
 from keras.layers import BatchNormalization, Dense, Dropout, Input
 from keras.models import Model
 
-param = {'D0':13, 'D1':8, 'Dropout': 0.2}
+param = {'Dense0':13, 'Dense1':8, 'Dropout': 0.2}
 
 def BinaryClassification(input_shape = None):
     inputs = Input(input_shape = input_shape, name = 'Input')
-    x = Dense(param['D0'], activation='leaky_relu', name = 'Dense0')(inputs)
+    x = Dense(param['Dense0'], activation='leaky_relu', name = 'Dense0')(inputs)
     x = Dropout(param['Dropout'])(x)
-    x = Dense(param['D1'], activation='leaky_relu', name = 'Dense1')(x)
+    x = Dense(param['Dense1'], activation='leaky_relu', name = 'Dense1')(x)
     outputs = Dense(1, activation='sigmoid', name = 'Output')(x)
     model = Model(inputs = inputs, outputs = outputs, name = 'TitanicBC')
     model.compile(loss = 'binary_crossentropy', optimizer='adam')
     model.summary()
-    
+
     return model
---
+++ process_func.py
@@ -0,0 +1,2 @@
+def Preprocess(data):
+  drop_labels = ['PassengerId', 'Cabin', 'Embarked']
